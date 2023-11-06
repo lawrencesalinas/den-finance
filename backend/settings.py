0529a5113f3f996337a2f50268546aa97cc63982
Lawrence <lawrencesalinas@yahoo.com>
2023-11-06 00:11:52 -0500

    updated requirements

--- backend/settings.py
+++ backend/settings.py
@@ -7,12 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/4.2/ref/settings/
+ps://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import dj_database_url
 import os
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()  # Take environment variables from .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -63,7 +65,9 @@ ROOT_URLCONF = "backend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, 'frontend/build')
+            ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -126,6 +130,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'frontend/build/static',
+    os.path.join(BASE_DIR, "frontend/build/static"),
+]
 
 STATIC_URL = "static/"
 
