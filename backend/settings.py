cfded705c602a27752e4a3756508ef5b1b4a689d
Lawrence <lawrencesalinas@yahoo.com>
2023-11-06 01:27:13 -0500

    updated requirements

--- backend/settings.py
+++ backend/settings.py
@@ -131,9 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "frontend/build"),
     BASE_DIR / 'static',
     BASE_DIR / 'frontend/build',
-    os.path.join(BASE_DIR, "frontend/build/static"),
+
 ]
 
 STATIC_URL = "static/"
